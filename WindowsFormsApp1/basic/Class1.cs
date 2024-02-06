444ac30aa701c08dbb274e666ee52012bd0ee870
abhishek.dubey <abhishek.dubey@INPOW-BVHXKW2>
2024-02-06 09:55:10 +0530

    Add project files.

---
+++ WindowsFormsApp1/basic/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Ram
+{
+
+    class laxman
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter your Number :");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            if (Number % 2 == 0)
+            {
+                Console.Write("This is Even Number :");
+            }
+            else
+            {
+                Console.Write("This is Odd Number");
+            }
+        }
+
+    }
+}
